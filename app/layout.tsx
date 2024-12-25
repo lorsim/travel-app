2ea95a64b437e6d3edf2e30d98a57d122d0aad60
lorsim <lorsim96@gmail.com>
2024-12-25 09:18:33 +0530

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-
-
 export const metadata: Metadata = {
   title: "Travel",
   description: "Travel  app for camping",
